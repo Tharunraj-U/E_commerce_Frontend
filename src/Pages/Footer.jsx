f66d537c5de54f448176a3b14edfe234c32fbfb8
tharun <tharunraj2023@.com>
2024-07-21 17:41:51 +0530

    final

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { FaWhatsapp } from 'react-icons/fa'; // Import WhatsApp icon
+import { FaWhatsapp, FaLinkedin } from 'react-icons/fa'; // Import WhatsApp and LinkedIn icons
 import '../Styles/Footer.css';
 
 const Footer = () => {
@@ -17,11 +17,18 @@ const Footer = () => {
     window.open('https://wa.me/7010791192', '_blank'); // Replace with your WhatsApp number
   };
 
+  const handleLinkedInClick = () => {
+    window.open('https://www.linkedin.com/in/tharun-raj-726360252?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app', '_blank'); // Replace with your LinkedIn profile URL
+  };
+
   return (
     <div className="footer">
-      <p className='p1'>&copy; {new Date().getFullYear()} Tharun Raj </p>
+      <p className='p1'>&copy; {new Date().getFullYear()} Tharun Raj</p>
       <p className="p1">{time}</p> {/* Display the current time */}
-      <FaWhatsapp className="whatsapp-icon" onClick={handleWhatsappClick} />
+      <div className="icon-container">
+        <FaWhatsapp className="whatsapp-icon" onClick={handleWhatsappClick} />
+        <FaLinkedin className="linkedin-icon" onClick={handleLinkedInClick} />
+      </div>
     </div>
   );
 };
